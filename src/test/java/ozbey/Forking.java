9854b98ee45b235fb9dcc1082254d9764643527e
OzbeyG <gokhanozbey@gmail.com>
2020-11-01 12:36:22 -0500

    second commit

--- src/test/java/ozbey/Forking.java
+++ src/test/java/ozbey/Forking.java
@@ -3,5 +3,6 @@ package ozbey;
 public class Forking {
     public static void main(String[] args) {
         System.out.println("this is first commit");
+        // and this is second commit
     }
 }
